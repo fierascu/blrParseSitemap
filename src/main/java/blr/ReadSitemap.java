b1cc214ff46fcd9468cf41f4da55bf77522abd9f
fierascu <fierascu@gmail.com>
2017-02-15 07:57:30 +0200

    if sitemap ends with .txt extension threat it as text file and read one line at a time and and return links

--- src/main/java/blr/ReadSitemap.java
+++ src/main/java/blr/ReadSitemap.java
@@ -7,37 +7,51 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ReadSitemap {
 
-    static Logger log = Logger.getLogger(App.class.getName());
+    private static Logger log = Logger.getLogger(ReadSitemap.class.getName());
 
     public static void main(String[] args) {
     }
 
     public static List getLinksFromSitemap(String sitemapFile) {
-        try {
-            File file = new File(sitemapFile);
-            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
-
-            NodeList nodeLinks = doc.getElementsByTagName("loc");
-            List links = new ArrayList<>();
-
-            for (int i = 0; i < nodeLinks.getLength(); i++) {
-                String usr = nodeLinks.item(i).getTextContent();
-                //log.trace(i + " " + usr);
-                if (Character.isDigit(usr.charAt(usr.length() - 1))) {
-                    links.add(usr);
-                }
+        List<String> links = new ArrayList<>();
+        if (sitemapFile.endsWith(".txt")) {
+            try (Stream<String> stream = Files.lines(Paths.get(sitemapFile))) {
+                stream
+                        .filter(f -> f.length() > 0 && Character.isDigit(f.charAt(f.length() - 1)))
+                        .forEach(links::add);
+            } catch (IOException e) {
+                log.error(e);
             }
-            log.trace("Hei, just read " + links.size() + " possible products links!");
+            log.trace("Hei, just read " + links.size() + " possible products links from " + sitemapFile);
             return links;
-        } catch (Exception e) {
-            log.error(e);
+        } else {
+            try {
+                File file = new File(sitemapFile);
+                Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+                NodeList nodeLinks = doc.getElementsByTagName("loc");
+
+                for (int i = 0; i < nodeLinks.getLength(); i++) {
+                    String usr = nodeLinks.item(i).getTextContent();
+                    if (usr.length() > 0 && Character.isDigit(usr.charAt(usr.length() - 1))) {
+                        links.add(usr);
+                    }
+                }
+                log.trace("Hei, just read " + links.size() + " possible products links from " + sitemapFile);
+                return links;
+            } catch (Exception e) {
+                log.error(e);
+            }
         }
         log.trace("NO LINKS READ!");
-        return new ArrayList<>();
+        return links;
     }
 }
